b0912370e55046875a78a41e31e605ee02d2c63c
Taha <tahanaeem12@hotmail.com>
2021-01-02 00:42:47 -0500

    added new units of measurements

--- lib/models/Exercise.dart
+++ lib/models/Exercise.dart
@@ -1,7 +1,14 @@
 class Exercise {
-  final String name, type, data, time, date;
-  final double weight;
+  final String name, type;
+  final double date; // unix time
+  final List<num> reps, time, distance, weight;
 
   Exercise(
-      {this.name, this.type, this.data, this.time, this.date, this.weight});
+      {this.name,
+      this.type,
+      this.time,
+      this.date,
+      this.weight,
+      this.reps,
+      this.distance});
 }
