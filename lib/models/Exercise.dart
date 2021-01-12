80b00e55b76259b39c019bcc4075ef2e12947fae
Taha <tahanaeem12@hotmail.com>
2021-01-12 14:20:33 -0500

    changed model fields

--- lib/models/Exercise.dart
+++ lib/models/Exercise.dart
@@ -1,13 +1,14 @@
 class Exercise {
-  final String name, type;
-  final double date; // unix time
+  final String name;
+  // final String type;
+  // final double date; // unix time
   final List<num> reps, time, distance, weight;
 
   Exercise(
       {this.name,
-      this.type,
+      // this.type,
       this.time,
-      this.date,
+      // this.date,
       this.weight,
       this.reps,
       this.distance});
